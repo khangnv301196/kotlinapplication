dffab6b5bc0b0763731d8308c09658fd988fe39e
khangnv301196 <59218853+khangnv301196@users.noreply.github.com>
2020-09-22 17:05:39 +0700

    update

---
+++ app/src/main/java/com/testing/kotlinapplication/ui/staff/StaffActivity.kt
@@ -0,0 +1,12 @@
+package com.testing.kotlinapplication.ui.staff
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.testing.kotlinapplication.R
+
+class StaffActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_staff)
+    }
+}
