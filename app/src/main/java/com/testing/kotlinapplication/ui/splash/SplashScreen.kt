1cb138b800c0d3b4b7380c7c3b5ff378fc160297
khang <khang.nguyen.301196@gmail.com>
2020-09-23 23:14:33 +0700

    update

--- app/src/main/java/com/testing/kotlinapplication/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/testing/kotlinapplication/ui/splash/SplashScreen.kt
@@ -7,12 +7,13 @@ import android.os.Looper
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.testing.kotlinapplication.MainActivity
+import com.testing.kotlinapplication.ui.staff.StaffActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
-            var intent = Intent(this, MainActivity::class.java)
+            var intent = Intent(this, StaffActivity::class.java)
             startActivity(intent)
         }, 3000)
     }
--- app/src/main/java/com/testing/kotlinapplication/ui/staff/statistic/SatisticFragment.kt
+++ app/src/main/java/com/testing/kotlinapplication/ui/staff/statistic/SatisticFragment.kt
@@ -1,11 +1,21 @@
 package com.testing.kotlinapplication.ui.staff.statistic
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis.XAxisPosition
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.testing.kotlinapplication.R
+import kotlinx.android.synthetic.main.fragment_satistic.*
 
 class SatisticFragment : Fragment() {
 
@@ -15,6 +25,76 @@ class SatisticFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_satistic, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpLineChart(linechart)
+        linechart.data = setLineChartData()
+        setUpPieChart(pieChart)
+        pieChart.data = setPieChartData()
+    }
+
+    fun setUpLineChart(lineChart: LineChart) {
+        var xAxis = lineChart.getXAxis()
+        xAxis.setPosition(XAxisPosition.BOTTOM)
+        xAxis.setDrawGridLines(false)
+        xAxis.setDrawAxisLine(true)
+
+        val leftAxis: YAxis = lineChart.getAxisLeft()
+        leftAxis.setLabelCount(5, false)
+        leftAxis.axisMinimum = 0f
+
+        lineChart.animateX(750)
+    }
+
+    fun setUpPieChart(pieChart: PieChart) {
+        pieChart.description.isEnabled = false
+        pieChart.holeRadius = 52f
+        pieChart.setTransparentCircleRadius(57f)
+//        pieChart.setCenterText(mCenterText)
+//        pieChart.setCenterTextTypeface(mTf)
+        pieChart.setCenterTextSize(9f)
+        pieChart.setUsePercentValues(true)
+        pieChart.setExtraOffsets(5f, 10f, 50f, 10f)
+
+        val l: Legend = pieChart.legend
+        l.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        l.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+        l.orientation = Legend.LegendOrientation.VERTICAL
+        l.setDrawInside(false)
+        l.yEntrySpace = 0f
+        l.yOffset = 0f
+
+        pieChart.animateY(900)
+    }
+
+    fun setLineChartData(): LineData {
+        var mList = ArrayList<Entry>()
+        for (i in 0..12) {
+            mList.add(Entry(i.toFloat(), ((Math.random() * 65) + 40).toFloat()))
+        }
+        var d1 = LineDataSet(mList, "New DataSet " + "zero" + ", (1)")
+        d1.lineWidth = 2.5f
+        d1.circleRadius = 4.5f
+        d1.highLightColor = Color.rgb(244, 117, 117)
+        d1.setDrawValues(false)
+        val sets = ArrayList<ILineDataSet>()
+        sets.add(d1)
+        return LineData(sets)
+    }
+
+    fun setPieChartData(): PieData {
+        var mList = ArrayList<PieEntry>()
+        for (i in 0..2) {
+            mList.add(PieEntry((Math.random() * 70 + 30).toFloat(), "Quater"))
+        }
+        val d = PieDataSet(mList, "")
+        // space between slices
+        d.sliceSpace = 2f
+        d.setColors(*ColorTemplate.VORDIPLOM_COLORS)
+
+        return PieData(d)
     }
 
 }
